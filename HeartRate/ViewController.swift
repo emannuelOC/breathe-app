a7f9813e5a0ced625b5e47fdc53c397edec41676
Emannuel Carvalho <emannuel.carvalho@concrete.com.br>
2018-11-21 14:09:08 -0200

    Initial Commit

---
+++ HeartRate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HeartRate
+//
+//  Created by emannuel.carvalho on 21/11/18.
+//  Copyright © 2018 emannuel.carvalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
